3fb5aa07ecb682648471d39b4c6748320975aec0
foxlo <foxlo@DESKTOP-4R8BSQ7>
2021-03-11 03:49:53 +0800

    JPA

--- src/test/java/jpa/single/Update.java
+++ src/test/java/jpa/single/Update.java
@@ -9,9 +9,9 @@ public class Update extends JPATemplate {
     @Test
     public void t1() {
         
-        // 1. 永續物件修改
-        Customer c1 = (Customer)session.get(Customer.class, 1);
-        c1.setLastName("AAA");
+//        // 1. 永續物件修改
+//        Customer c1 = (Customer)session.get(Customer.class, 1);
+//        c1.setLastName("AAA");
         
         // 2. 新創物件修改
         Customer c2 = new Customer();
@@ -19,14 +19,14 @@ public class Update extends JPATemplate {
         c2.setLastName("BBB");
         session.update(c2); // 若未加此行, 不會修改 -> 因為是"新創物件"
         
-        // 3. 下達 SQL 修改
-        String sql = "UPDATE Customer set last_name = :lastName WHERE id = :id";
-        Query query = session.createQuery(sql);
-        // Query query = session.createSQLQuery(sql); 
-        // 不知道是用哪個?  createQuery(sql) or .createSQLQuery(sql)
-        query.setParameter("lastName", "CCC");
-        query.setParameter("id", 3);
-        int updateRowCount = query.executeUpdate(); // 回傳一個異動筆數
-        System.out.println("updateRowCount = " + updateRowCount);
+//        // 3. 下達 SQL 修改
+//        String sql = "UPDATE Customer set last_name = :lastName WHERE id = :id";
+//        Query query = session.createQuery(sql);
+//        // Query query = session.createSQLQuery(sql); 
+//        // 不知道是用哪個?  createQuery(sql) or .createSQLQuery(sql)
+//        query.setParameter("lastName", "CCC");
+//        query.setParameter("id", 3);
+//        int updateRowCount = query.executeUpdate(); // 回傳一個異動筆數
+//        System.out.println("updateRowCount = " + updateRowCount);
     }
 }
